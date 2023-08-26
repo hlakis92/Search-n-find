38e44493183b42c2253096677bba2701f2d71502
Hassan Lakis <lakis004@umn.edu>
2023-08-26 16:52:31 -0500

    tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the scavenger hunt instructions', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const instructionsElement = screen.getByText(/Welcome to the Scavenger Hunt game!/i);
+  expect(instructionsElement).toBeInTheDocument();
+});
+
+test('renders the hint paragraph', () => {
+  render(<App />);
+  const hintText = "A bent limb touching earth, displays of affection and a long-lasting commitment?";
+  const hintElement = screen.getByText(hintText);
+  expect(hintElement).toBeInTheDocument();
+});
+
+
+test('renders the clear all button', () => {
+  render(<App />);
+  const clearAllButton = screen.getByRole('button', { name: /Clear All/i });
+  expect(clearAllButton).toBeInTheDocument();
 });
